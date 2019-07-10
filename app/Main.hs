e90da302485ccd44b304ca2d6d013c0d9f43cf70
4afS <g2x649@gmail.com>
2019-07-10 20:35:15 +0900

    rename type alias

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
 import Brainfxck
+import Control.Monad
+import Control.Monad.Trans
+import System.Console.Haskeline
+
+type Repl a = InputT IO a
 
 main :: IO ()
-main = run ">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-]<.>+++++++++++[<+++++>-]<.>++++++++[<+++>-]<.+++.------.--------.[-]>++++++++[<++++>-]<+.[-]++++++++++."
+main = runInputT defaultSettings repl
+
+repl :: Repl ()
+repl = do
+  safeInput <- getInputLine ">> "
+  case safeInput of
+    Just ":q" -> outputStrLn "see you later."
+    Just s -> liftIO (run s) >> repl
+    Nothing -> outputStrLn "Please Enter a code" >> repl
--- src/Brainfxck.hs
+++ src/Brainfxck.hs
@@ -21,8 +21,8 @@ data Op
   | Loop Ops
   deriving (Eq, Show)
 
-type Watching = Int
-type PointerSequence = (Vector Int, Watching)
+type Memory = (Vector Int, Pointer)
+type Pointer = Int
 
 type Ops = Vector Op
 
@@ -51,18 +51,18 @@ uncons :: Vector a -> Maybe (a, Vector a)
 uncons (V.null -> True) = Nothing
 uncons vs               = Just (V.head vs, V.tail vs)
 
-toEvaluable :: Ops -> StateT PointerSequence IO (Maybe String)
+toEvaluable :: Ops -> StateT Memory IO (Maybe String)
 toEvaluable op = fmap (map chr) . sequence . filter (/= Nothing) <$> toEvaluable' op
   where
-    toEvaluable' :: Ops -> StateT PointerSequence IO [Maybe Int]
+    toEvaluable' :: Ops -> StateT Memory IO [Maybe Int]
     toEvaluable' Nil                 = return []
     toEvaluable' (Cons (Loop op) xs) = loop op ++^ toEvaluable' xs
     toEvaluable' (Cons x         xs) = evaluate x +^ toEvaluable' xs
 
-    loop :: Ops -> StateT PointerSequence IO [Maybe Int]
+    loop :: Ops -> StateT Memory IO [Maybe Int]
     loop op = do
-      (seq, watching) <- get
-      if seq V.! watching > 0
+      (memory, pointer) <- get
+      if memory V.! pointer > 0
          then toEvaluable' op ++^ loop op
          else return []
 
@@ -72,38 +72,38 @@ mx +^ mxs = (:) <$> mx <*> mxs
 (++^) :: Monad m => m [a] -> m [a] -> m [a]
 mxs ++^ mys = (++) <$> mxs <*> mys
 
-evaluate :: Op -> StateT PointerSequence IO (Maybe Int)
-evaluate MoveRight = modify (updateWatching increment) >> return Nothing
+evaluate :: Op -> StateT Memory IO (Maybe Int)
+evaluate MoveRight = modify (updatePointer increment) >> return Nothing
 
-evaluate MoveLeft = modify (updateWatching decrement) >> return Nothing
+evaluate MoveLeft = modify (updatePointer decrement) >> return Nothing
 
 evaluate Increment = do
-  (_, watching) <- get
-  modify $ updatePointerSequence watching increment
+  (_, pointer) <- get
+  modify $ updateMemory pointer increment
   return Nothing
 
 evaluate Decrement = do
-  (_, watching) <- get
-  modify $ updatePointerSequence watching decrement
+  (_, pointer) <- get
+  modify $ updateMemory pointer decrement
   return Nothing
 
 evaluate Put = do
-  (seq, watching) <- get
-  return . Just $ seq V.! watching
+  (memory, pointer) <- get
+  return . Just $ memory V.! pointer
 
 evaluate Substitution = do
-  (_, watching) <- get
+  (_, pointer) <- get
   c             <- lift getChar
-  modify $ updatePointerSequence watching (const (ord c))
+  modify $ updateMemory pointer (const (ord c))
   return Nothing
 
-updatePointerSequence
-  :: Watching -> (Int -> Int) -> PointerSequence -> PointerSequence
-updatePointerSequence index f (seq, watching) =
-  (seq V.// [(index, f (seq V.! index))], watching)
+updateMemory
+  :: Pointer -> (Int -> Int) -> Memory -> Memory
+updateMemory index f (memory, pointer) =
+  (memory V.// [(index, f (memory V.! index))], pointer)
 
-updateWatching :: (Watching -> Watching) -> PointerSequence -> PointerSequence
-updateWatching f (seq, watching) = (seq, f watching)
+updatePointer :: (Pointer -> Pointer) -> Memory -> Memory
+updatePointer f (memory, pointer) = (memory, f pointer)
 
 increment, decrement :: Int -> Int
 increment = (+ 1)
@@ -111,7 +111,7 @@ decrement = subtract 1
 
 run :: String -> IO ()
 run source = do
-  let initializedPointerSequence = V.replicate 30000 0
+  let initializedMemory = V.replicate 30000 0
   let evaluable = toEvaluable $ parse source
-  evaluated <- evalStateT evaluable (initializedPointerSequence, 0)
-  mapM_ putStrLn evaluated 
+  evaluated <- evalStateT evaluable (initializedMemory, 0)
+  mapM_ putStrLn evaluated
